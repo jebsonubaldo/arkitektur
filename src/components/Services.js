a3d2feb781cf38fcd216ed5cf793534e8b0316e6
Jebson Ubaldo <ubaldojebson@gmail.com>
2023-09-15 19:51:41 +0800

    feat: update Services section

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,25 +6,25 @@ export default function Example() {
         <p className="mb-4 text-md leading-8 text-gray-600">
           Our Services
         </p>
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We Shape the Life of Tomorrow</h2>
+        <h2 className="text-3xl font-bold tracking-tight text-[#006D5B] sm:text-4xl">We Shape the Life of Tomorrow</h2>
       </div>
       <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      <div className="bg-gray-400/5 p-8 sm:p-10">
+          <div className="bg-gray-400/5 p-8 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">01 Architecture</h3>
           </div>
-          <div className="bg-gray-400/5 p-6 sm:p-10">
+          <div className="bg-gray-400/5 p-6 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">02 Landscape Design</h3>
           </div>
-          <div className="bg-gray-400/5 p-6 sm:p-10">
+          <div className="bg-gray-400/5 p-6 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">03 Engineering</h3>
           </div>
-          <div className="bg-gray-400/5 p-6 sm:p-10">
+          <div className="bg-gray-400/5 p-6 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">04 Interior Design</h3>
           </div>
-          <div className="bg-gray-400/5 p-6 sm:p-10">
+          <div className="bg-gray-400/5 p-6 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">05 Graphic Design</h3>
           </div>
-          <div className="bg-gray-400/5 p-6 sm:p-10">
+          <div className="bg-gray-400/5 p-6 sm:p-10 hover:bg-[#006D5B] hover:text-white">
             <h3 class="text-center">06 Consultancy</h3>
           </div>
       </div>
