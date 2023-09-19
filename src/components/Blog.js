419f6b369714f1eb100e3bb402a89dab58aa6122
unknown <johncarlolipa@gmail.com>
2023-09-19 19:19:28 +0800

    fix error blog

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -54,7 +54,12 @@ export const getStaticProps = async (context) => {
   }
 };
 
-export default function Example() {
+export default function Example({ post }) {
+  // Define formattedContent based on the post prop
+  const formattedContent = post
+    ? addSpacesBeforeH4AndParagraphs(post.content.rendered)
+    : "";
+
   return (
     <div className="bg-white pb-24">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -66,65 +71,67 @@ export default function Example() {
             Learn how to grow your business with our expert advice.
           </p>
           <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
-            <article
-              key={post.id}
-              className="relative isolate flex flex-col gap-8 lg:flex-row"
-            >
-              <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-                <Image
-                  src={post.x_featured_media}
-                  alt={post.title.rendered}
-                  width={0}
-                  height={0}
-                  className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
-                />
-                <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-              </div>
-              <div>
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {new Date(post.date).toDateString()}
-                  </time>
-                  <a
-                    href="#"
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.categories}
-                  </a>
+            {post && ( // Check if post is defined before rendering
+              <article
+                key={post.id}
+                className="relative isolate flex flex-col gap-8 lg:flex-row"
+              >
+                <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+                  <Image
+                    src={post.x_featured_media}
+                    alt={post.title.rendered}
+                    width={0}
+                    height={0}
+                    className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+                  />
+                  <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
                 </div>
-                <div className="group relative max-w-xl">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href="#">
-                      <span className="absolute inset-0" />
-                      {post.title.rendered}
+                <div>
+                  <div className="flex items-center gap-x-4 text-xs">
+                    <time dateTime={post.datetime} className="text-gray-500">
+                      {new Date(post.date).toDateString()}
+                    </time>
+                    <a
+                      href="#"
+                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                    >
+                      {post.categories}
                     </a>
-                  </h3>
-                  <p className="mt-5 text-sm leading-6 text-gray-600">
-                    {parse(formattedContent)}
-                  </p>
-                </div>
-                <div className="mt-6 flex border-t border-gray-900/5 pt-6">
-                  <div className="relative flex items-center gap-x-4">
-                    <Image
-                      src={post.x_featured_media}
-                      alt={post.title.rendered}
-                      width={0}
-                      height={0}
-                      className="h-10 w-10 rounded-full bg-gray-50"
-                    />
-                    <div className="text-sm leading-6">
-                      <p className="font-semibold text-gray-900">
-                        <a href="#">
-                          <span className="absolute inset-0" />
-                          {post.x_author}
-                        </a>
-                      </p>
-                      <p className="text-gray-600">{post.x_author}</p>
+                  </div>
+                  <div className="group relative max-w-xl">
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                      <a href="#">
+                        <span className="absolute inset-0" />
+                        {post.title.rendered}
+                      </a>
+                    </h3>
+                    <p className="mt-5 text-sm leading-6 text-gray-600">
+                      {parse(formattedContent)}
+                    </p>
+                  </div>
+                  <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+                    <div className="relative flex items-center gap-x-4">
+                      <Image
+                        src={post.x_featured_media}
+                        alt={post.title.rendered}
+                        width={0}
+                        height={0}
+                        className="h-10 w-10 rounded-full bg-gray-50"
+                      />
+                      <div className="text-sm leading-6">
+                        <p className="font-semibold text-gray-900">
+                          <a href="#">
+                            <span className="absolute inset-0" />
+                            {post.x_author}
+                          </a>
+                        </p>
+                        <p className="text-gray-600">{post.x_author}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </article>
+              </article>
+            )}
           </div>
         </div>
       </div>
