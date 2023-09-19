e437a184dd6501aded3559b5c1eb0458a917c10b
unknown <johncarlolipa@gmail.com>
2023-09-19 20:06:40 +0800

    add post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import Blog from "@/components/Blog";
 import Footer from "@/components/Footer";
 import Details from "./blogs/[id]";
 
@@ -11,7 +9,7 @@ export default function Home() {
   return (
     <div className={`${inter.className}`}>
       <Navbar />
-      <Details />
+      <Details post={post} />
       <Footer />
     </div>
   );
